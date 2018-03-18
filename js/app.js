1e8f9de5c968a2960061a2eefeb47261589ee247
Pedro Souza <posouza@gmail.com>
2018-03-18 19:52:57 -0300

    corrections to CheckCollisions Function

--- js/app.js
+++ js/app.js
@@ -9,79 +9,104 @@ var Enemy = function(x, y) {
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    var sprite = 'images/enemy-bug.png';
     this.x = x;
     this.y = y;
-};
+    
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    if (this.x < 505) {
-        this.x += (150 * dt);
+    this.getPositionX = function() {
+        return this.x;
     }
-    else {this.x = -90;}
-
-    // If the enemy and the player collide.
-    if(this.x < player.x + 30 && this.x + 60 > player.x && this.y < player.y + 60 && this.y + 40 > player.y) {
-        score = 0;
-        document.getElementById('playerScore').innerHTML = score;
-        player.reset();
+    this.getPositionY = function() {
+        return this.y;
     }
+       
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    this.update = function(dt) {
+        // You should multiply any movement by the dt parameter
+        // which will ensure the game runs at the same speed for
+        // all computers.
+        if (this.x < 505) {
+            this.x += (150 * dt);
+        }
+        else {
+            this.x = -90;
+        }    
+        console.log(player.getPositionX());
+        // If the enemy and the player collide.
+        if(this.x < player.getPositionX() + 30 && this.x + 60 > player.getPositionX() && this.y < player.getPositionY() + 60 && this.y + 40 > player.getPositionY()) {
+            score = 0;
+            document.getElementById('playerScore').innerHTML = score;
+            player.reset();
+        }
+    };
+
+    // Draw the enemy on the screen, required method for game
+    this.render = function() {
+        ctx.drawImage(Resources.get(sprite), this.x, this.y);
+    };
+
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
 var Player = function () {
-    this.sprite = 'images/char-boy.png';
-    this.x = 200;
-    this.y = 320;
-};
-
-// Is called every time the player position is updated
-Player.prototype.update = function() {
     
-    // If the player reaches the water
-    if (player.y < 20) {
-    score++;
-    document.getElementById('playerScore').innerHTML = score;
-    this.reset();
-}
-};
+    var sprite = 'images/char-boy.png';
+    var position = {x : 200, y : 300};
 
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    this.getPositionX = function() {
+        return position.x;
+    };
 
-Player.prototype.handleInput = function(direction) {
-    if(direction == 'left' && this.x > 0) {
-        this.x -= 50;
+    this.getPositionY = function() {
+        return position.y;
     }
-    if(direction == 'right' && this.x < 400) {
-        this.x += 50;
-    }
-    if(direction == 'up' && this.y > 3) {
-        this.y -= 50;
-    }
-    if(direction == 'down' && this.y < 400) {
-        this.y += 50;
-    }
-};
 
-// Is called when the player is reset to the starting point
-Player.prototype.reset = function() {
-    this.x = 200;
-    this.y = 320;
+    this.setPosition = function(x, y) {
+        position.x = x;
+        position.y = y;
+    }
+
+    // Is called every time the player position is updated
+    this.update = function() {
+    
+        // If the player reaches the water
+        if (position.y < 20) {
+        score++;
+        document.getElementById('playerScore').innerHTML = score;
+        this.reset();
+    }
+    };
+
+    this.render = function() {
+        ctx.drawImage(Resources.get(sprite), position.x, position.y);
+    };
+
+    this.handleInput = function(direction) {
+        if(direction == 'left' && position.x > 0) {
+            position.x -= 50;
+        }
+        if(direction == 'right' && position.x < 400) {
+            position.x += 50;
+        }
+        if(direction == 'up' && position.y > 3) {
+            position.y -= 50;
+        }
+        if(direction == 'down' && position.y < 400) {
+            position.y += 50;
+        }
+    };
+
+    // Is called when the player is reset to the starting point
+    this.reset = function() {
+        this.setPosition(200, 300);
+    };
+
 };
 
 // Now instantiate your objects.
@@ -97,8 +122,6 @@ var allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5, enemy6];
 // Place the player object in a variable called player
 var player = new Player();
 
-
-
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
@@ -111,3 +134,4 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
